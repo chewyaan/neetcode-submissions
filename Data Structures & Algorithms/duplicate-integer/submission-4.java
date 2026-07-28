99970f908dc34df4827015f97123d4d100fc9ed4
Enzo Panem <90363014+chewyaan@users.noreply.github.com>
2026-07-27 20:10:10 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> uniqueNum = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (uniqueNum.contains(nums[i])) {
+                return true;
+            }
+            uniqueNum.add(nums[i]);
+        }
+        return false;
+    }
+}
